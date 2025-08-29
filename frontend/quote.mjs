0f339d61179c6ccab4121ce996cfe954d45142e4
Gabriel <deng93003@gmail.com>
2025-08-29 14:46:50 +0100

    New updates on the app

--- frontend/quote.mjs
+++ frontend/quote.mjs
@@ -18,6 +18,42 @@ const displayQuotes = (data) => {
   
 }
 
+// Submit new quote to the server
+
+const submitQuote = async (event) => {
+  event.preventDefault();
+
+  const quote = document.getElementById('quote-inpt').value.trim();
+  const author = document.getElementById('author-inpt').value.trim();
+
+  if (!quote || !author) {
+    alert("Please fill in both fields, thanks..");
+    return;
+  }
+
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({ quote, author })
+    });
+
+    if (response.ok) {
+      alert("Quote added successfully! Yahhh");
+      document.getElementById('new-quote-form').reset();
+      getQuote();
+    }
+    else {
+      alert("Failed to add quote.");
+    }
+  }
+  catch (error) {
+    console.error("Error submitting quote:". error);
+  }
+};
+
+document.getElementById('new-quote-form').addEventListener('submit', submitQuote);
+
 document.getElementById('new-quote').addEventListener('click', getQuote);
 
 window.onload = getQuote();
