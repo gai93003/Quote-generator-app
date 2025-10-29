15765a1971bca14813187f32afa76aac0b606f44
Gabriel <deng93003@gmail.com>
2025-10-29 20:10:57 +0000

    These are the files that whree i made some changes

--- frontend/quote.mjs
+++ frontend/quote.mjs
@@ -1,7 +1,4 @@
-// const url = 'https://cs4cc48o8wwskkwk8oss0884.hosting.codeyourfuture.io/api/quotes';
-// const url = 'https://gai93003-quote-generator.hosting.codeyourfuture.io/';
-
-const url = 'http://localhost:3000'
+const url = 'https://gai93003-quote-generator.hosting.codeyourfuture.io/';
 
 const getQuote = async () => {
   try {
